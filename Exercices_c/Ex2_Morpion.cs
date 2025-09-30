96785ed47f1bb33df861ebc313a8bdd949e33b61
BOUTAHRI123 <Siham.boutahri@gmail.com>
2025-09-30 16:48:06 +0200

    Push ex 2 et 3

---
+++ Exercices_c/Ex2_Morpion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serie2
+{
+    public static class Morpion
+    {
+        /*typeGrille grille : je vais utiliser un tableau de type char et de taille 3*3 */
+        public static void DisplayMorpion(char[,] tab)
+        {
+            int i;
+            int j;
+            Console.WriteLine("Affichage grille de Morpion :");
+            for (i=0;i < tab.GetLength(0);i++)
+            {
+                for (j = 0; j < tab.GetLength(1); j++)
+                {
+                    Console.Write($"{tab[i,j]} ");
+                }
+                Console.WriteLine();
+            }
+            return;
+        }
+
+        public static int CheckMorpion(/*typeGrille grille */)
+        {
+            //TODO
+            return -1;
+        }
+    }
+}
---
+++ Exercices_c/Ex3_Search.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serie2
+{
+    public static class Search
+    {
+        public static int LinearSearch(int[] tableau, int valeur)
+        {
+            int i;
+            int index = -1;
+            for (i = 0; i < tableau.Length; i++)
+            {
+                if (tableau[i] == valeur)
+                {
+                    index = i;
+                }
+                
+            }
+            if(index != -1)
+            {
+                Console.WriteLine($"L'indice est : {index} , Valeur trouvé ");
+            }
+            else
+            {
+                Console.WriteLine($"L'indice est : {index} , Valeur introuvable !");
+            }
+            
+            return index;
+        }
+
+        public static int BinarySearch(int[] tableau, int valeur)
+        {
+            //TODO
+            return -1;
+        }
+    }
+}
